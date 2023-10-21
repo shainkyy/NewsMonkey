fbbbb93c16664f3096d9ce10113f22b9c1348c36
shainkyy <35163727+shainkyy@users.noreply.github.com>
2023-10-21 23:31:14 +0530

    added js code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
+    const pathurl = window.location.pathname;
     return (
       <>
         <nav className="navbar navbar-expand-lg bg-dark text-light">
@@ -27,80 +28,177 @@ export default class Navbar extends Component {
               className="collapse navbar-collapse text-light"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
+              {window.screen.width <= 768 ? (
+                <div
+                  className="scroll d-flex justify-content-between"
+                  style={{ width: `${window.screen.width - 30}px` }}
+                >
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/"
                   >
                     Home
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/business" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/business"
                   >
                     Business
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/entertainment" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/entertainment"
                   >
                     Entertainment
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/general" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/general"
                   >
                     General
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/health" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/health"
                   >
                     Health
                   </Link>
-                </li>
-                <li className="nav-item">
+
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/science" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/science"
                   >
                     Science
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/sports" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/sports"
                   >
                     Sports
                   </Link>
-                </li>
-                <li className="nav-item">
                   <Link
-                    className="nav-link text-light active"
+                    className={`nav-link  spaceInMobile ${
+                      pathurl === "/technology" ? "active" : ""
+                    }`}
                     aria-current="page"
                     to="/technology"
                   >
                     Technology
                   </Link>
-                </li>
-              </ul>
+                </div>
+              ) : (
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/business" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/business"
+                    >
+                      Business
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/entertainment" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/entertainment"
+                    >
+                      Entertainment
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/general" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/general"
+                    >
+                      General
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/health" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/health"
+                    >
+                      Health
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/science" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/science"
+                    >
+                      Science
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/sports" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/sports"
+                    >
+                      Sports
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link text-light spaceInDesk ${
+                        pathurl === "/technology" ? "active" : ""
+                      }`}
+                      aria-current="page"
+                      to="/technology"
+                    >
+                      Technology
+                    </Link>
+                  </li>
+                </ul>
+              )}
             </div>
           </div>
         </nav>
